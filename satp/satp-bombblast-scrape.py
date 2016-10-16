8ab73cc1b846ebce79e3a4d468c759f385be1c13
SarahArmstrong <armstrong.sarah.kathleen@gmail.com>
2016-10-16 20:03:35 +0500

    Got table entries into a list type

--- satp/satp-bombblast-scrape.py
+++ satp/satp-bombblast-scrape.py
@@ -4,9 +4,22 @@ from bs4 import BeautifulSoup
 
 def make_soup(url):
 	page = urllib.request.urlopen(url)
-	sdata = BeautifulSoup(page, "html.parser")
+	sdata = BeautifulSoup(page, 'html.parser')
 	return sdata
 
-url = "http://www.satp.org/satporgtp/countries/pakistan/database/bombblast.htm"
+url = 'http://www.satp.org/satporgtp/countries/pakistan/database/bombblast.htm'
 
 soup = make_soup(url)
+
+table = soup.findAll('table', attrs={'class':'pagraph1'})
+table = table[0]
+
+
+trows = table.findAll('tr')
+bbdata = []
+for trow in trows:
+    bbdata_ = trow.findAll('td')
+    bbdata_ = [ele.text.strip() for ele in bbdata_]
+    bbdata.append(bbdata_)
+    
+print(bbdata)
