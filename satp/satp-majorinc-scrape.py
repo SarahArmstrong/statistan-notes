27cf415ba784db426179f9edce1464620fa5af9f
SarahArmstrong <armstrong.sarah.kathleen@gmail.com>
2016-10-17 22:02:12 +0500

    loop for years 2013 - 2015 added

--- satp/satp-majorinc-scrape.py
+++ satp/satp-majorinc-scrape.py
@@ -51,3 +51,45 @@ title = soup.title.text.strip()
 year = title[-4:]
 
 df.to_csv('midata' + str(year), sep=',')
+
+# ii. 2013 - Present year_{-1}
+
+present_year = 2016
+for yr in range(2013, present_year, 1):
+
+    url = 'http://www.satp.org/satporgtp/countries/pakistan/database/majorincidents' + str(yr) + '.htm'
+
+    soup = make_soup(url)
+
+    table = soup.findAll('table')
+    table = table[2]
+
+    trows = table.findAll('tr')
+    midata = []
+
+    midata_h1 = trows[0].findAll('td')
+    midata_h1 = [ele.text.strip() for ele in midata_h1]
+
+    midata_h2 = trows[1].findAll('td')
+    midata_h2 = [ele.text.strip() for ele in midata_h2]
+
+    for trow in trows[2:]:
+        midata_body = trow.findAll('td')
+        midata_body = [ele.text.strip() for ele in midata_body]
+        midata.append(midata_body)
+
+    sub_headers = []
+    for i in range(0, 4, 1):
+        sub_headers_ = str(midata_h2[i] + " " + midata_h1[3])
+        sub_headers.append(sub_headers_)
+
+    headers = midata_h1[0:3] + sub_headers + midata_h1[4:]
+
+    df = pd.DataFrame(midata, columns = headers)
+
+    df = df.ix[:, 1:8] # Drop index 0 column
+
+    title = soup.title.text.strip()
+    year = title[-4:]
+
+    df.to_csv('midata' + str(year), sep=',')
