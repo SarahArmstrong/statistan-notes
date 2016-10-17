5849feccd339cc0c7b35e4565ee15c47483414e7
SarahArmstrong <armstrong.sarah.kathleen@gmail.com>
2016-10-17 23:19:06 +0500

    script now includes 2007 and onwards - need to scrape for 1988 - 2006

--- satp/satp-majorinc-scrape.py
+++ satp/satp-majorinc-scrape.py
@@ -11,7 +11,9 @@ def make_soup(url):
 	sdata = BeautifulSoup(page, 'html.parser')
 	return sdata
 
+
 # (1) Table Type 1: 2013 - Present Year
+
 
 # i. Present year
 
@@ -51,6 +53,7 @@ title = soup.title.text.strip()
 year = title[-4:]
 
 df.to_csv('midata' + str(year), sep=',')
+
 
 # ii. 2013 - Present year_{-1}
 
@@ -93,3 +96,85 @@ for yr in range(2013, present_year, 1):
     year = title[-4:]
 
     df.to_csv('midata' + str(year), sep=',')
+
+
+# Table Type 2: 2007 - 2012; NOTE: Data unstructured - needs to be manually translated to be usable
+
+
+# 2012:
+
+url = 'http://www.satp.org/satporgtp/countries/pakistan/database/majorincidents2012.htm'
+
+soup = make_soup(url)
+
+table = soup.findAll('table')
+table = table[1]
+
+trows = table.findAll('tr')
+midata = []
+
+for trow in trows[2:3]:
+    midata_body = trow.findAll('td')
+    midata_body = [ele.text.strip() for ele in midata_body]
+    midata.append(midata_body)
+midata = [val for sublist in midata for val in sublist][2]
+
+midata = midata.replace('\r\n', '')
+midata = midata.replace('\n\n\n\n\n\n\n', '')
+midata = midata.replace('           ', ' ')
+midata = midata.split('\n')
+
+title = midata[0]
+midata = midata[2:]
+
+for i in range(0, len(midata), 1):
+    midata[i] = midata[i].split(':')
+midata = [val for sublist in midata for val in sublist]
+
+df = pd.DataFrame(midata)
+
+year = title[-4:]
+
+df.to_csv('midata' + str(year), sep=',')
+
+
+# 2007 - 2011
+
+for yr in range(2007, 2012, 1):
+
+    url = 'http://www.satp.org/satporgtp/countries/pakistan/database/majorinci' + str(yr) + '.htm'
+
+    soup = make_soup(url)
+
+    table = soup.findAll('table')
+    table = table[1]
+
+    trows = table.findAll('tr')
+    midata = []
+
+    for trow in trows[2:3]:
+        midata_body = trow.findAll('td')
+        midata_body = [ele.text.strip() for ele in midata_body]
+        midata.append(midata_body)
+    midata = [val for sublist in midata for val in sublist][2]
+
+    midata = midata.replace('\r\n', '')
+    midata = midata.replace('\n\n\n\n\n\n\n', '')
+    midata = midata.replace('           ', ' ')
+    midata = midata.split('\n')
+
+    title = midata[0]
+    midata = midata[2:]
+
+    for i in range(0, len(midata), 1):
+        midata[i] = midata[i].split(':')
+    midata = [val for sublist in midata for val in sublist]
+
+    df = pd.DataFrame(midata)
+
+    year = title[-4:]
+
+    df.to_csv('midata' + str(year), sep=',')
+
+
+
